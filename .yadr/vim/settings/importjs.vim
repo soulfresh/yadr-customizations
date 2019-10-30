86ea347ac51bfce0cc863023c2f686b83551b49c
Marc Wren <things@soul-fresh.com>
2019-10-29 18:45:43 -0600

    Moved most yadr vim setting overrides into .vimrc.after.

--- .yadr/vim/settings/importjs.vim
+++
@@ -1,8 +0,0 @@
-" Import the file under the cursor
-noremap <Space>i :ImportJSWord<CR>
-
-" Add/Cleanup imports
-noremap <Space>f :ImportJSFix<CR>
-
-" Go to the package under cursor
-noremap <Space>g :ImportJSGoto<CR>
--- customize.sh
+++ customize.sh
@@ -1,12 +1,14 @@
 #!/bin/sh
 
 echo 'moving files...'
+cp .gitconfig.user ~/
+cp .my.cnf ~/
 cp .tmux.conf.user ~/
 cp .vimrc.after ~/
+cp .agignore ~/
 cp .zsh.after/* ~/.zsh.after/
 cp .zsh.prompts/* ~/.zsh.prompts/
 cp .yadr/vim/.vundles.local ~/.yadr/vim/
 cp -rf .yadr/vim/* ~/.yadr/vim/
-cp .my.cnf ~/
 echo 'complete'
 
