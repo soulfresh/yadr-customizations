b0d38583cbbdfa0738565e57fb19a0348d942dc8
Marc Wren <things@soul-fresh.com>
2020-03-22 11:44:06 -0600

    Global copy/paste and other small tweaks.

--- .zsh.after/aliases-marc.zsh
+++ .zsh.after/aliases-marc.zsh
@@ -12,14 +12,3 @@ alias m4a2wav='for mp3 in ./*.m4a; do ffmpeg -i "$mp3" -acodec pcm_s16le -ac 1 -
 alias wav2mp3='for wav in ./*.wav; do ffmpeg -i "$wav" -q:a 0 "${$(basename $wav)}".mp3; done'
 alias aif2mp3='for wav in ./*.aif; do ffmpeg -i "$wav" -q:a 0 "${$(basename $wav)}".mp3; done'
 alias wav2m4a='for wav in ./*.wav; do ffmpeg -i "$wav" -codec:a aac "${$(basename $wav)}".m4a; done'
-
-# POWER PRO
-## Google Cloud Services
-### Start the proxy server to connect to prod
-alias proxy-prod='cloud_sql_proxy -instances=power-pro:us-central1:powerpro-5-7-replica=tcp:3307 -credential_file=$HOME/.ssh/PowerPro\ Production-b402787bc6ad.json &'
-### Startup mysql to use the proxy server to connect to prod
-alias mysql-replica="mysql -hcloudsql-replica -P3307 -p -umarc_ro"
-
-# Import Communities
-alias ci="~/Development/powerpro/breach/scripts/community-import --source-host 127.0.0.1 --source-port 3307 --source-user marc_ro --source-pass Ira12_lookup"
-alias cid="ci -gc -d"
--- .zsh.after/powerpro.zsh
+++ .zsh.after/powerpro.zsh
@@ -5,4 +5,14 @@ PATH=$PATH:/usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk:/usr/loc
 # Run `powerpro-proxy-dev` to setup the proxy that allows connecting to the DB remotely.
 # Run `powerpro-mysql-dev` to connect to the dev environment DB.
 alias powerpro-proxy-dev='cloud_sql_proxy -verbose=false -instances=power-pro-dev:us-central1:powerpro-dev-5-7=tcp:3309 -credential_file=$HOME/.ssh/power-pro-dev-8831b256c650.json &'
-alias powerpro-mysql-dev="mysql -h127.0.0.1 -P3309 -p -umarc_rw"
+alias powerpro-mysql-dev="mysql -h127.0.0.1 -P3309 -p"
+
+## Google Cloud Services
+### Start the proxy server to connect to prod
+alias powerpro-proxy-prod='cloud_sql_proxy -instances=power-pro:us-central1:powerpro-5-7-replica=tcp:3307 -credential_file=$HOME/.ssh/PowerPro\ Production-b402787bc6ad.json &'
+### Startup mysql to use the proxy server to connect to prod
+alias powerpro-mysql-replica="mysql -hcloudsql-replica -P3307 -p -umarc_ro"
+
+# Import Communities
+alias ci="~/Development/powerpro/breach/scripts/community-import --source-host 127.0.0.1 --source-port 3307 --source-user marc_ro --source-pass Ira12_lookup"
+alias cid="ci -gc -d"
