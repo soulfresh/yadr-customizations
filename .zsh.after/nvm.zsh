08c117795c5dc6fdaa2886d3c7bf88af8a89c31c
Marc Wren <things@soul-fresh.com>
2020-12-17 10:39:50 -0700

    Upgrade nvm usage per nvm docs.

--- .zsh.after/nvm.zsh
+++ .zsh.after/nvm.zsh
@@ -1,9 +1,5 @@
-export NVM_DIR="$HOME/.nvm"
-. "$(brew --prefix nvm)/nvm.sh"
-
-# export NVM_DIR="$HOME/.nvm"
-#   [ -s "/usr/local/opt/nvm/nvm.sh" ] && . "/usr/local/opt/nvm/nvm.sh"  # This loads nvm
-#   [ -s "/usr/local/opt/nvm/etc/bash_completion.d/nvm" ] && . "/usr/local/opt/nvm/etc/bash_completion.d/nvm"  # This loads nvm bash_completion
+export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
 
 # place this after nvm initialization!
 autoload -U add-zsh-hook
--- setup-node.sh
+++ setup-node.sh
@@ -1,5 +1,5 @@
-# install nvm
-brew install nvm
+# install nvm (Homebrew is unsupported)
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.37.2/install.sh | bash
 
 # install the latest node
 nvm install --lts
